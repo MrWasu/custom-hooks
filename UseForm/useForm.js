a6a45c74fd9f3751827023280277e02a0dd9f6dc
Juan Jesus <juanj.cesar@gmail.com>
2023-03-23 14:04:27 +0100

    useForm mejorado

--- UseForm/useForm.js
+++ UseForm/useForm.js
@@ -1,31 +1,67 @@
-import { useState } from 'react' 
+import { useEffect, useMemo, useState } from 'react';
 
-export const useForm = ( initialForm = {} ) => {
+export const useForm = (initialForm = {}, formValidations = {}) => {
 
-    const [formState, setFormState] = useState( initialForm )
-   
-    //const { username, email, password } = formState
+    const [formState, setFormState] = useState(initialForm);
+    const [formValidation, setFormValidation] = useState({});
 
+    
+    const isFormValid = useMemo(() => {
+        
+        // importa el resultado de las validaciones, las comprueba, y luego exporta un boolean
+        for (const formValue of Object.keys(formValidation)) {
+            if (formValidation[formValue] !== null) return false;
+        }
+        
+        return true;
+    }, [formValidation])
+    
+    
     const onInputChange = ({ target }) => {
-
-        const { name, value } = target
-
+        // funcion que se encarga de cambiar el estado al escribri dentro del campo
+        const { name, value } = target;
         setFormState({
             ...formState,
-            [name]: value,
-        })
+            [name]: value
+        });
     }
-
+    
     const onResetForm = () => {
+        setFormState(initialForm);
+    }
+    
+    
+    // funciones que se encargan de verificar las validaciones cada vez que el estado de los campos de texto cambien
+    useEffect(() => {
+        createValidators();
+    }, [formState])
 
-        setFormState( initialForm )
+    const createValidators = () => {
+
+        const formCheckedValues = {};
+
+        for (const formField of Object.keys(formValidations)) {
+            const [fn, errorMessage] = formValidations[formField];
+            
+            formCheckedValues[`${formField}Valid`] = fn(formState[formField]) ? null : errorMessage;
+        }
+        /*  const formValidations = { // 
+             email: [ (value) => value.includes('@'), 'El correo debe de tener una @'],
+             password: ... // código del componente que se esta iterando en esta función
+           }
+          */
+
+        setFormValidation(formCheckedValues);
     }
 
     return {
+        ...formState,
         formState,
         onInputChange,
         onResetForm,
-        ...formState, // se manda desesctucturado (email, name, password)
+
+        ...formValidation,
+        isFormValid
     }
 }
 
